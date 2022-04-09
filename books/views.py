facfd2504ecacc53b271705dee12912c30ea07de
Krzysztof Kotarski <Chris@woka.com>
2022-04-09 15:46:09 +0200

    aa

--- books/views.py
+++ books/views.py
@@ -1,3 +1,9 @@
+from django.core.handlers.wsgi import WSGIRequest
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def get_hello(request: WSGIRequest) -> HttpResponse:
+	return HttpResponse("hello world")
+
+def get_uuids_a(request: WSGIRequest) -> HttpResponse:
+	return HttpResponse("test")
