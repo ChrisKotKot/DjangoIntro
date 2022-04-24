ef17be2f0da66c2b2506c9e2d37dd6c5e9cd7a7e
Krzysztof Kotarski <Chris@woka.com>
2022-04-24 15:58:30 +0200

    Added AuthorCreateView class

--- books/urls.py
+++ books/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from books.views import get_uuids_a, get_uuids_b, get_argument_from_path, get_argument_from_query, \
     check_http_query_type, get_headers, raise_error_for_fun, AuthorListBaseView, CategoryListTemplateView, \
-    BooksListView, BookDetailsView, CategoryCreateFormView
+    BooksListView, BookDetailsView, CategoryCreateFormView, AuthorCreateView
 
 urlpatterns = [
     path('uuids-a', get_uuids_a),
@@ -12,10 +12,11 @@ urlpatterns = [
     path('query-types', check_http_query_type, name="check_query_type"),
     path('get-headers', get_headers, name="get_headers"),
     path('raise-error', raise_error_for_fun, name='raise-error'),
-    path('author-list', AuthorListBaseView.as_view(), name='author-list'),
+    path('author-list', AuthorListBaseView.as_view(), name='author_list'),
     path('category-list', CategoryListTemplateView.as_view(), name='category_list'),
     path('books-list', BooksListView.as_view(), name='books_list'),
     path('book-details/<int:pk>', BookDetailsView.as_view(), name='books_details'),
     path('category-create', CategoryCreateFormView.as_view(), name='category_create'),
+    path('author-create', AuthorCreateView.as_view(), name='author_create'),
 
 ]
