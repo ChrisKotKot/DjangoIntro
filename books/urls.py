300a3573b471bd8e7f79eed73294e84480a16b94
Krzysztof Kotarski <Chris@woka.com>
2022-05-14 10:09:22 +0100

    DeleteView added.

--- books/urls.py
+++ books/urls.py
@@ -1,25 +1,26 @@
 from django.urls import path
 
-from books.views import get_uuids_a, get_uuids_b, get_argument_from_path, get_argument_from_query, \
-    check_http_query_type, get_headers, raise_error_for_fun, AuthorListBaseView, CategoryListTemplateView, \
-    BooksListView, BookDetailsView, CategoryCreateFormView, AuthorCreateView, AuthorUpdateView, BookCreateView
+from books.views import get_uuids_a, get_uuids_b, get_argument_from_path, get_arguments_from_query, \
+    check_http_query_types, get_headers, raise_error_for_fun, AuthorListBaseView, CategoryListTemplateView, \
+    BookListView, BookDetailsView, CategoryCreateFormView, AuthorCreateView, AuthorUpdateView, BookCreateView, \
+    BookUpdateView, BookDeleteView
 
 urlpatterns = [
     path('uuids-a', get_uuids_a),
     path('uuids-b', get_uuids_b),
     path('path-args/<int:x>/<str:y>/<slug:z>/', get_argument_from_path, name="get_from_path"),
-    path('query-args', get_argument_from_query, name="get_from_query"),
-    path('query-types', check_http_query_type, name="check_query_type"),
+    path('query-args/<int:x>/<str:y>/<slug:z>/', get_arguments_from_query, name="get_from_query"),
+    path('query-types', check_http_query_types, name="check_query_types"),
     path('get-headers', get_headers, name="get_headers"),
-    path('raise-error', raise_error_for_fun, name='raise-error'),
-    path('author-list', AuthorListBaseView.as_view(), name='author_list'),
-    path('category-list', CategoryListTemplateView.as_view(), name='category_list'),
-    path('books-list', BooksListView.as_view(), name='books_list'),
-    path('books-create', BookCreateView.as_view(), name='book_create'),
-    path('book-details/<int:pk>', BookDetailsView.as_view(), name='books_details'),
-    path('category-create', CategoryCreateFormView.as_view(), name='category_create'),
-    path('author-create', AuthorCreateView.as_view(), name='author_create'),
-    path('author-update/<int:pk>', AuthorUpdateView.as_view(), name='author_update'),
-    path('author-update', AuthorUpdateView.as_view(), name='author_update_name'),
-
+    path('raise-error', raise_error_for_fun, name="raise_error"),
+    path('authors-list', AuthorListBaseView.as_view(), name="authors_list"),
+    path('category-list', CategoryListTemplateView.as_view(), name="category_list"),
+    path('books-list', BookListView.as_view(), name="books_list"),
+    path('book-create', BookCreateView.as_view(), name="book_create"),
+    path('book-update/<int:pk>/', BookUpdateView.as_view(), name="book_update"),
+    path('book-delete/<int:pk>/', BookDeleteView.as_view(), name="book_delete"),
+    path('book-details/<int:pk>/', BookDetailsView.as_view(), name="books_details"),
+    path('category-create/', CategoryCreateFormView.as_view(), name="category_create"),
+    path('author-create/', AuthorCreateView.as_view(), name="author_create"),
+    path('author-update/<int:pk>/', AuthorUpdateView.as_view(), name="author_update"),
 ]
--- books/views.py
+++ books/views.py
@@ -3,24 +3,26 @@ from uuid import uuid4
 from django.core.exceptions import BadRequest
 from django.core.handlers.wsgi import WSGIRequest
 from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
-from django.shortcuts import render, get_object_or_404
-from django.views.generic import TemplateView, ListView, DetailView, FormView, CreateView, UpdateView
+from django.views.generic import TemplateView, ListView, DetailView, FormView, CreateView, UpdateView, DeleteView
 
-from books.forms import CategoryForm, AuthorForm, BookForm
 from books.models import BookAuthor, Category, Book
 import logging
-logger = logging.getLogger("chris")
+
+from books.forms import CategoryForm, AuthorForm, BookForm
+
+logger = logging.getLogger("Ala")
 
 
 class AuthorListBaseView(View):
-	template_name = "author_list.html"
+	template_name = "authors_list.html"
 	queryset = BookAuthor.objects.all()  # type: ignore
 
 	def get(self, request: WSGIRequest, *args, **kwargs):
-		logger.debug(f"{request} !!!")
+		logger.debug(f"{request} ---")
 		context = {"authors": self.queryset}
 		return render(request, template_name=self.template_name, context=context)
 
@@ -30,18 +32,19 @@ class CategoryListTemplateView(TemplateView):
 	extra_context = {"categories": Category.objects.all()}  # type: ignore
 
 
-class BooksListView(ListView):
+class BookListView(ListView):
 	template_name = "books_list.html"
 	model = Book
 	paginate_by = 10
 
 
 class BookDetailsView(DetailView):
-	template_name = "book_detail.html"
+	template_view = "book_detail.html"
 	model = Book
 
 	def get_object(self, **kwargs):
 		return get_object_or_404(Book, id=self.kwargs.get("pk"))
+
 
 class CategoryCreateFormView(FormView):
 	template_name = "category_form.html"
@@ -49,7 +52,7 @@ class CategoryCreateFormView(FormView):
 	success_url = reverse_lazy("category_list")
 
 	def form_invalid(self, form):
-		logger.critical(f"FORM CRITICAL ERROR, More info:{form}")
+		logger.critical(f"FORM CRITICAL ERROR, MORE INFO {form}")
 		return super().form_invalid(form)
 
 	def form_valid(self, form):
@@ -60,35 +63,63 @@ class CategoryCreateFormView(FormView):
 		logger.info(f"check_entity-id={check_entity.id}")
 		return result
 
+
 class AuthorCreateView(CreateView):
 	template_name = "author_form.html"
 	form_class = AuthorForm
-	success_url = reverse_lazy("author_list")
+	success_url = reverse_lazy("authors_list")
+
 
 class AuthorUpdateView(UpdateView):
 	template_name = "author_form.html"
 	form_class = AuthorForm
-	success_url = reverse_lazy("author_update")
+	success_url = reverse_lazy("authors_list")
 
 	def get_object(self, **kwargs):
 		return get_object_or_404(BookAuthor, id=self.kwargs.get("pk"))
 
-# 11.
-
-def get_hello(request: WSGIRequest) -> HttpResponse:
-	hello = "Hello world!"
-	return render(request, template_name="Hello_world.html", context={"hello_var": hello})
 
 class BookCreateView(CreateView):
 	template_name = "book_form.html"
 	form_class = BookForm
-	success_url =reverse_lazy("books_create")
+	success_url = reverse_lazy("books_list")
 
-# 12.
+
+# def get_success_url(self):
+#     return reverse_lazy("book_list")
+
+class BookUpdateView(UpdateView):
+	template_name = "book_form.html"
+	form_class = BookForm
+	success_url = reverse_lazy("books_list")
+
+	def get_object(self, **kwargs):
+		return get_object_or_404(Book, id=self.kwargs.get("pk"))
+
+
+class BookDeleteView(DeleteView):
+	template_name = "book_delete.html"
+	model = Book
+	success_url = reverse_lazy("books_list")
+
+	def get_object(self, **kwargs):
+		return get_object_or_404(Book, id=self.kwargs.get("pk"))
+
+
+def get_hello(request: WSGIRequest) -> HttpResponse:
+	hello = "hello world"
+	return render(request, template_name="hello_world.html", context={"hello_var": hello})
+
+
+# 12. Utwórz funkcję zwracającą listę stringów. Stringi niech będą losowym UUID dodawanym do listy. Lista niech posiada 10 elementów.
+#
+#     a) Zwróć listę jako HTTPResponse (musisz na liście zrobić json.dumps)
+#     b) zwróć listę jako JsonResponse
+
 
 def get_uuids_a(request: WSGIRequest) -> HttpResponse:
 	uuids = [f"{uuid4()}" for _ in range(10)]
-	return render(request, template_name="uuids_a.html", context={"elements": uuids})
+	return render(request, template_name="uuids.html", context={"elements": uuids})
 	# return HttpResponse(f"uuids={uuids}")
 
 
@@ -97,51 +128,46 @@ def get_uuids_b(request: WSGIRequest) -> JsonResponse:
 	return JsonResponse({"uuids": uuids})
 
 
-# 13.
-
 def get_argument_from_path(request: WSGIRequest, x: int, y: str, z: str) -> HttpResponse:
-	return HttpResponse(f"x = {x}, y = {y}. z = {z}")
+	return HttpResponse(f"X = {x}, Y = {y}, Z = {z}")
 
 
-# 14.
-
-def get_argument_from_query(request: WSGIRequest, x: int, y: str, z: str) -> HttpResponse:
+def get_arguments_from_query(request: WSGIRequest) -> HttpResponse:
 	a = request.GET.get("a")
 	b = request.GET.get("b")
 	c = request.GET.get("c")
-	print(type(int(a)))
-	return HttpResponse("Test")
+	print(type(int("a")))
+	return HttpResponse(f"a = {a}, b = {b}, c = {c}")
 
 
-# 15.
+# 15. Przygotuj funkcję drukująca odpowiedni komunikat dla method HTTP takich jak GET, POST, PUT, DELETE
 
 @csrf_exempt
-def check_http_query_type(request: WSGIRequest) -> HttpResponse:
-	# query_type = "Unknown"
-	# if request.method == "GET":
-	#    query_type = "To jest GET"
-	# elif request.method == "PAST":
-	#     query_type = "To jest PAST"
-	# elif request.method == "PUT":
-	#     query_type = "To jest PUT"
-	# elif request.method == "DELETE":
-	#     query_type = "To jest DELETE"
-	# return HttpResponse(query_type)
-	return render(request, template_name="method.html", context={})
+def check_http_query_types(request: WSGIRequest) -> HttpResponse:
+	query_type = "Unknown"
+	#    if request.method == "GET":
+	#        query_type = "This is GET"
+	#    elif request.method == "POST":
+	#        query_type = "This is POST"
+	#    elif request.method == "This is PUT":
+	#        query_type = "This is PUT"
+	#    elif request.method == "This is DELETE":
+	#        query_type = "This is DELETE"
+	#    return HttpResponse(query_type)
+	return render(request, template_name="methods.html", context={})
 
 
-# 21.
-
-def get_headers(request: WSGIRequest) -> HttpResponse:
+# 21. Przygotuj funkcję, która zwróci informację o headerach HTTP
+def get_headers(request: WSGIRequest) -> JsonResponse:
 	our_headers = request.headers
 
 	return JsonResponse({"headers": dict(our_headers)})
 
 
-# 22.
+# 22. Rzuć wyjątkiem HTTP
 
 @csrf_exempt
 def raise_error_for_fun(request: WSGIRequest) -> HttpResponse:
 	if request.method != "GET":
-		raise BadRequest("method not allowes")
-	return HttpResponse("Wszystko GIT")
+		raise BadRequest("Method not allowed")
+	return HttpResponse("Everything is OK")
